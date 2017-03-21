40353e146bdcb0144c0a1fa23d6e3e51e7dde015
sarony <saron.yitbarek@gmail.com>
2017-03-21 02:58:50 -0400

    Creates dynamic speaker pages

--- config.rb
+++ config.rb
@@ -13,8 +13,11 @@ page '/*.txt', layout: false
 # page "/path/to/file.html", layout: :otherlayout
 
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
+data.people.speakers.each do |name|
+  first_name = name.full_name.split(" ").first.downcase
+  proxy "/speakers/#{first_name}", "/speakers/template.html", locals: { speaker: name
+  }
+end
 
 # General configuration
 
@@ -42,3 +45,4 @@ configure :build do
   # Minify Javascript on build
   # activate :minify_javascript
 end
+
